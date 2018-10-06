92499884dccbd82f3a409a87f49553d5d8a931f3
Alex Morales Garcia <alex2521999@github.com>
2018-10-07 00:26:12 +0200

    Added Colliders cpp and h

---
+++ Dev_class5_handout/Motor2D/j1Collision.h
@@ -0,0 +1,62 @@
+#ifndef __J1COLLISION_H__
+#define __J1COLLISION_H__
+
+#define MAX_COLLIDERS 300
+
+#include "j1Module.h"
+#include "SDL\include\SDL_rect.h"
+
+enum COLLIDER_TYPE
+{
+	COLLIDER_NONE = -1,
+	COLLIDER_GROUND,
+	COLLIDER_PLAYER,
+
+	COLLIDER_MAX
+};
+
+struct Collider
+{
+	SDL_Rect rect;
+	bool to_delete = false;
+	COLLIDER_TYPE type;
+	j1Module* callback = nullptr;
+
+	Collider(SDL_Rect rectangle, COLLIDER_TYPE type, j1Module* callback = nullptr) :
+		rect(rectangle),
+		type(type),
+		callback(callback)
+	{}
+
+	void SetPos(int x, int y)
+	{
+		rect.x = x;
+		rect.y = y;
+	}
+
+	bool CheckCollision(const SDL_Rect& r) const;
+};
+
+class j1Collision : public j1Module
+{
+public:
+
+	j1Collision();
+	~j1Collision();
+
+	bool PreUpdate();
+	bool Update();
+	//update_status PostUpdate();
+	bool CleanUp();
+
+	Collider* AddCollider(SDL_Rect rect, COLLIDER_TYPE type, j1Module* callback = nullptr);
+	void DebugDraw();
+
+private:
+
+	Collider* colliders[MAX_COLLIDERS];
+	bool matrix[COLLIDER_MAX][COLLIDER_MAX];
+	bool debug = false;
+};
+
+#endif
