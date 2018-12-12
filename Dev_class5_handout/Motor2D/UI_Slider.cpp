fb76a25127d888600d893f89b42df0f3e6e6cc96
Laia <36154539+LaiaMartinezMotis@users.noreply.github.com>
2018-12-12 12:40:53 +0100

    Added new buttons

--- Dev_class5_handout/Motor2D/UI_Slider.cpp
+++ Dev_class5_handout/Motor2D/UI_Slider.cpp
@@ -1,21 +1,40 @@
 #include "j1App.h"
-#include "UI_Slider.h"
-#include "UI_GUI.h"
+#include "p2Log.h"
+#include "j1Gui.h"
+#include "j1Window.h"
+#include "j1Scene.h"
+#include "j1SceneMenu.h"
+#include "j1Input.h"
 #include "j1Render.h"
 #include "j1Fonts.h"
+#include "UI_Slider.h"
+#include "UI_Button.h"
 
-UI_Slider::UI_Slider()
+UI_Slider::UI_Slider(SDL_Rect rect, Slider_type type)
 {
+	slider_rect = { 593,182,24,24 };
+	slider_hover = { 593,182,24,24 };
+	slider_click = { 619,182,24,24 };
+
+	slider_type = type;
+	sprite_rect = rect;
+	
 }
 
 bool UI_Slider::Start()
 {
-	return false;
+	App->render->Blit(App->gui->GetAtlas(), pos.x, pos.y, &sprite_rect);
+	return true;
 }
 
 bool UI_Slider::PostUpdate()
 {
-	return false;
+	bool ret = false;
+
+	ret = App->render->Blit(App->gui->GetAtlas(), pos.x, pos.y, &slider_rect);
+
+
+	return ret;
 }
 
 //void UI_Slider::OnClick()
--- Dev_class5_handout/Motor2D/UI_Slider.h
+++ Dev_class5_handout/Motor2D/UI_Slider.h
@@ -2,24 +2,31 @@
 #define _UI_SLIDER__H
 
 #include "p2Defs.h"
-#include "UI_GUI.h"
+#include "j1Gui.h"
 #include "SDL/include/SDL.h"
 
-
+enum Slider_type {
+	SLIDER
+};
 class UI_Slider: public UI_GUI
 {
 public:
-	UI_Slider();
+	UI_Slider(SDL_Rect rect, Slider_type type);
 	~UI_Slider() {};
 
 	bool Start();
 	bool PostUpdate();
 	/*void OnClick();*/
 
-private:
-	
 
-public:
+private:
+	SDL_Rect slider_rect;
+	SDL_Rect slider_hover;
+	SDL_Rect slider_click;
+
+
+	Slider_type	slider_type;
+	SDL_Rect sprite_rect;
 	
 };
 
