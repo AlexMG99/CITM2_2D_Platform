23e60a675736bac81acf71f206f5008ab3aae309
Alex Morales Garcia <alex2521999@github.com>
2018-12-16 12:53:00 +0100

    Solved Bug

--- Dev_class5_handout/Motor2D/j1Scene_Credits.cpp
+++ Dev_class5_handout/Motor2D/j1Scene_Credits.cpp
@@ -36,11 +36,14 @@ bool j1Scene_Credits::Start()
 {
 	
 	bool ret = true;
-	
+	App->render->camera = { 0,0 };
+
 	credits.create("MIT License		Copyright(c)[2018][Alex Morales & Laia Martinez] Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the Software), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions : The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
 	SDL_Rect button_rect[3] = { { 705,20,115,29 }, { 705,51,115,29 }, { 705,82,115,29 } };
 	App->gui->CreateButton({ 18,215 }, Button_Type::BACK, button_rect[0], &button_rect[1], &button_rect[2], "Button", true);
 	
+	App->gui->Start();
+
 	return ret;
 }
 
